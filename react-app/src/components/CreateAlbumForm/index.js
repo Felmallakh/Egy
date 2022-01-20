164d6b971817e8e12f950a4b24a3b9e6561e3c83
felmallakh <fady_elmallakh@hotmail.com>
2022-01-19 16:33:19 -0800

    create done

--- react-app/src/components/CreateAlbumForm/index.js
+++ react-app/src/components/CreateAlbumForm/index.js
@@ -1,13 +1,12 @@
 import React, { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { Navigate, useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 import { getAlbumsThunk, addAlbumThunk } from "../store/album";
 
-
 function CreateAlbumForm() {
   const dispatch = useDispatch();
-  const history = useNavigate();
+  const hist = useNavigate();
 
   const session = useSelector((state) => state.session.user);
 
@@ -15,50 +14,55 @@ function CreateAlbumForm() {
   const [description, setDescription] = useState("");
   const [errors, setErrors] = useState([]);
 
-
-
   // Handle submit function
   const addAlbum = async (e) => {
     e.preventDefault();
     const userId = session.id;
 
     await dispatch(addAlbumThunk({ userId, title, description }));
+    hist(`/users/${userId}/albums`);
   };
 
-
   return (
-    <>
-      <nav className="form-nav">
-        <div  id="logo-container">
-          <span className="form-logoText" id="home-logoText">
-
-          </span>
+    <div className="authwrapper">
+      <div className="auth">
+        <div className="namelogo">
+          <Link to="/">
+            <img
+              src="https://cdn.discordapp.com/attachments/919391399269515305/932090523496370277/logo-removebg-preview.png"
+              alt="logo"
+            ></img>
+          </Link>
         </div>
-      </nav>
-      <div id="createAlbum-form-background">
-        <form
-          onSubmit={addAlbum}
-          className="form-container"
-          id="createAlbum-form-container"
-        >
-          <div className="form-header">
-            <div className="form-headerText">Create Album</div>
-          </div>
-          {errors.length > 0 && (
-            <ul className="errors-container">
-              {errors.map((error, idx) => (
-                <li className="error" key={idx}>
-                  {error}
-                </li>
-              ))}
-            </ul>
-          )}
-          <div className="field-container">
+        <p>Create New Album</p>
+        <ul>
+          {errors.map((error, idx) => (
+            <li className="errors" key={idx}>
+              {error}
+            </li>
+          ))}
+        </ul>
+        <div id="createAlbum-form-background">
+          <form
+            onSubmit={addAlbum}
+            className="form-container"
+            id="createAlbum-form-container"
+          >
+            {errors.length > 0 && (
+              <ul className="errors-container">
+                {errors.map((error, idx) => (
+                  <li className="error" key={idx}>
+                    {error}
+                  </li>
+                ))}
+              </ul>
+            )}
             <input
               className="form-field"
               type="text"
               placeholder="Title"
               value={title}
+              required
               onChange={(e) => setTitle(e.target.value)}
             />
             <input
@@ -68,17 +72,11 @@ function CreateAlbumForm() {
               value={description}
               onChange={(e) => setDescription(e.target.value)}
             />
-            <button
-              id="createAlbum-form-button"
-              className="form-button"
-              type="submit"
-            >
-              Create Album
-            </button>
-          </div>
-        </form>
+            <button type="submit">Create Album</button>
+          </form>
+        </div>
       </div>
-    </>
+    </div>
   );
 }
 
