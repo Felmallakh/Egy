109e54e28ba458e09cad5eef55228494374e8525
felmallakh <fady_elmallakh@hotmail.com>
2022-01-19 18:48:49 -0800

    style album

--- react-app/src/components/Albums/AlbumPreview.js
+++ react-app/src/components/Albums/AlbumPreview.js
@@ -1,21 +1,20 @@
-import React from 'react';
-import { NavLink } from 'react-router-dom';
-import "./albums.css"
+import React from "react";
+import { NavLink } from "react-router-dom";
+import "./albums.css";
 
 const AlbumDetail = ({ album }) => {
-
-    return (
-      <div className="album-container">
-        <NavLink className="link" exact to={`/albums/${album.id}`}>
-          <img
-            className="album-preview-image"
-            src="https://cdn.discordapp.com/attachments/919391399269515305/933267234263007262/King-Tut-removebg-preview.png"
-            alt="post preview"
-            />
-            <h2 className="album-title">{album.title}</h2>
-        </NavLink>
-      </div>
-    );
+  return (
+    <li className="albums-preview-item-div">
+      <NavLink exact to={`/albums/${album.id}`}>
+        <img
+          className="albums-preview-image"
+          src="https://cdn.discordapp.com/attachments/919391399269515305/933267234263007262/King-Tut-removebg-preview.png"
+          alt="post preview"
+        />
+        <h2 className="album-title">{album.title}</h2>
+      </NavLink>
+    </li>
+  );
 };
 
 export default AlbumDetail;
