8db794fa57b0f811fd2079757e05c0baf434aef1
felmallakh <fady_elmallakh@hotmail.com>
2022-01-20 21:24:36 -0800

    style

--- react-app/src/components/NotFound/index.js
+++ react-app/src/components/NotFound/index.js
@@ -1,14 +1,22 @@
-import "./notfound.css";
 import React from "react";
+import { useNavigate } from "react-router-dom";
+import "./notfound.css";
+
 
 function NotFound() {
+  const hist = useNavigate();
+
+  const back = (e) => {
+    e.preventDefault();
+    hist(`/`);
+  };
 
     return (
       <div id="not-found">
         <nav className="Hieroglyphics">
           <div class="container">
             <h1>An error has occured</h1>
-            <a href="/">Go back</a>
+            <button className="not-back" onClick={back}>Go back</button>
           </div>
         </nav>
       </div>
