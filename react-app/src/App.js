98accd81685ca4d0f999bde2e05bf861035ac7df
felmallakh <fady_elmallakh@hotmail.com>
2022-01-22 01:28:28 -0800

    profile

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -8,6 +8,7 @@ import { useDispatch } from "react-redux";
 import { authenticate } from "./components/store/session";
 import NotFound from "./components/NotFound";
 import Homepage from "./components/Homepage/Homepage"
+import Profile from "./components/Profile"
 import Albums from "./components/Albums/Albums"
 import Photos from "./components/Photos/Photos"
 import CreateAlbumForm from "./components/CreateAlbumForm"
@@ -38,6 +39,7 @@ function App() {
       <Route path="/signup" element={<SignUp />}></Route>
       <Route path="/NotFound" element={<NotFound />}></Route>
       <Route path="/home" element={<Homepage />}></Route>
+      <Route path="/users/:userId" element={<Profile />}></Route>
       <Route path="/users/:userId/albums" exact="true" element={<Albums />}></Route>
       <Route path="/users/:userId/photos" exact element={<Photos />}></Route>
       <Route path="/albums/new" element={<CreateAlbumForm />}></Route>
--- react-app/src/components/store/index.js
+++ react-app/src/components/store/index.js
@@ -1,13 +1,14 @@
 import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk'
 import session from './session'
+import profile from "./profile"
 import albumReducer from "./album";
 import photoReducer from "./photo";
 
 
-
 const rootReducer = combineReducers({
   session,
+  profile,
   albumReducer,
   photoReducer,
 });
