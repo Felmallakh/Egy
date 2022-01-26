75ce7e5b3d30cc614bb743b8b681da20695444ca
felmallakh <fady_elmallakh@hotmail.com>
2022-01-26 01:24:55 -0800

    titel

--- react-app/src/components/Albums/Albums.js
+++ react-app/src/components/Albums/Albums.js
@@ -40,10 +40,10 @@ function Albums() {
       <nav className="album-nav">
         <div className="album-left-Nav">
           <button id="signout" onClick={() => hist(`/home`)}>
-            Back
+            <i id="nav-size" className="fas fa-arrow-left"></i>
           </button>
-          <button id="signout" onClick={() => hist(`/albums/new`)}>
-            Create Album
+          <button class="nav-add" onClick={() => hist(`/albums/new`)}>
+              Add Album  <i className="fas fa-plus"></i>
           </button>
         </div>
         <div className="searchContainer">
@@ -54,13 +54,13 @@ function Albums() {
             Add Album
           </button> */}
           <button
-            id="signout"
+            class="nav-logout"
             onClick={async () => {
               await dispatch(logout());
               hist("/");
             }}
           >
-            Log Out
+            <i className="fas fa-sign-out-alt"></i> Log Out
           </button>
         </div>
       </nav>
