4522d87fed679661a28effb3ee79685597a97f69
felmallakh <fady_elmallakh@hotmail.com>
2022-01-26 10:49:39 -0800

    comments

--- app/forms/__init__.py
+++ app/forms/__init__.py
@@ -2,3 +2,4 @@ from .login_form import LoginForm
 from .signup_form import SignUpForm
 from .album_form import AlbumForm, EditAlbumForm
 from .photo_form import PhotoForm, EditPhotoForm
+from .comment_form import CommentForm, EditCommentForm
--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,2 +1,2 @@
 from .db import db
-from .models import User, Photo, Album, Favorite, Tag
+from .models import User, Photo, Album, Comment, Favorite, Tag
