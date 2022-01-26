d61fc365f5472d6b62da9a4cfed2fe4513145069
felmallakh <fady_elmallakh@hotmail.com>
2022-01-26 00:31:15 -0800

    titel

--- react-app/src/components/Photos/EditPhoto.js
+++ react-app/src/components/Photos/EditPhoto.js
@@ -19,8 +19,8 @@ function EditPhotoFrom() {
   const { photoId } = useParams();
   const photo = photos?.[photoId];
   const [title, setTitle] = useState(photo?.title);
-   const userId = user?.id;
-   const id = photoId;
+  const userId = user?.id;
+  const id = photoId;
   const [errors, setErrors] = useState([]);
 
   const editPhoto = async (e) => {
@@ -54,41 +54,40 @@ function EditPhotoFrom() {
         ></div>
       )}
       {showForm && (
-        <form
-          className="editForm"
-          onSubmit={editPhoto}
-        >
-          <div className="form1">
-            <h2>Edit Photo Title</h2>
-            <label>Photo title</label>
-            <input
-              placeholder={"Photo Title"}
-              type="text"
-              value={title}
-              onChange={(e) => {
-                setTitle(e.target.value);
-              }}
-              required
-            ></input>
-          </div>
-          <div id="editPhotoButton">
-            <div className="delete" onClick={handleSubmit}>
-              Delete <i className="fas fa-trash-alt"></i>
+        <div id="edit-container">
+          <form className="editForm" onSubmit={editPhoto}>
+            <div className="form1">
+              <h2>Edit Photo Title</h2>
+              <label>Photo title</label>
+              <input
+                placeholder={"Photo Title"}
+                type="text"
+                value={title}
+                onChange={(e) => {
+                  setTitle(e.target.value);
+                }}
+                required
+              ></input>
             </div>
-            <p
-              className="cancel"
-              onClick={(e) => {
-                dispatch(editPhotoOff());
-                setTitle("");
-              }}
-            >
-              Cancel
-            </p>
-            <button className="submit" disabled={!title}>
-              Submit
-            </button>
-          </div>
-        </form>
+            <div id="editPhotoButton">
+              <div className="delete" onClick={handleSubmit}>
+                Delete <i className="fas fa-trash-alt"></i>
+              </div>
+              <p
+                className="cancel"
+                onClick={(e) => {
+                  dispatch(editPhotoOff());
+                  setTitle("");
+                }}
+              >
+                Cancel
+              </p>
+              <button className="submit" disabled={!title}>
+                Submit
+              </button>
+            </div>
+          </form>
+        </div>
       )}
     </>
   );
