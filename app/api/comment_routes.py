d8380cd819f34c1fd49c4ef63717ae9260881bfc
felmallakh <fady_elmallakh@hotmail.com>
2022-01-26 09:50:29 -0800

    comments

---
+++ app/api/comment_routes.py
@@ -0,0 +1,17 @@
+from flask import Blueprint, request
+from flask_login import login_required, current_user
+from app.api.auth_routes import validation_errors_to_error_messages
+from app.models import db, Comment
+
+comment_routes = Blueprint('comments', __name__)
+
+# Delete Comment
+@comment_routes.route('/<int:commentId>', methods=['DELETE'])
+@login_required
+def deleteComment(commentId):
+    comment = Comment.query.filter_by(id= commentId).first()
+
+    db.session.delete(comment)
+    db.session.commit()
+
+    return comment.to_dict()
