fd00c402825d21e6792ec50daf432b9f09d4a4c3
felmallakh <fady_elmallakh@hotmail.com>
2022-01-27 01:04:49 -0800

    commentstyle

--- react-app/src/components/Photos/EditPhoto.js
+++ react-app/src/components/Photos/EditPhoto.js
@@ -1,4 +1,4 @@
-import React, { useState, useRef, useEffect } from "react";
+import React, { useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { useParams, useNavigate } from "react-router";
 import { updatePhotoThunk, deletePhotoThunk } from "../store/photo";
