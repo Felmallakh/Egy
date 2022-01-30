235d4373c44d503cd03f7acbe1cc1bd254f519cb
felmallakh <fady_elmallakh@hotmail.com>
2022-01-29 22:42:05 -0800

    comments done

--- react-app/src/components/Comments/EditComment.js
+++ react-app/src/components/Comments/EditComment.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { useParams, useNavigate } from "react-router";
 import { editCommentThunk, deleteCommentThunk } from "../store/comments";
