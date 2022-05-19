d7a22caf663e064900e77a229ba9f5da64b13604
felmallakh <fady_elmallakh@hotmail.com>
2022-05-18 22:49:47 -0700

    tfa

--- react-app/src/components/TFA/tfa.js
+++ react-app/src/components/TFA/tfa.js
@@ -1,10 +1,20 @@
 import React from "react";
+import QRCode from "react-google-qrcode";
+import './tfa.css'
+
 
 function TFA () {
 
     return (
+      <div className="container">
         <div>TFA Authentication</div>
-    )
+        <div>
+          <QRCode data="https://www.google.com" size={130} framed />
+        </div>
+      </div>
+    );
 }
 
 export default TFA;
+
+
